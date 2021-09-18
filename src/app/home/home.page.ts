7c2a0827af2c3974cfd3352225094ff8fea808ec
Jasa-Hollow <joel2262000@gmail.com>
2021-09-18 15:23:23 -0600

    FirstAppIonic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,26 @@ export class HomePage {
 
   constructor() {}
 
+  
+  /* -----------------------------*/
+  /* Agregar texto */
+  text = "Tengo hambre";
+  onTextChange() {
+    let app = document.getElementById('myStyle');
+    const p = document.createElement('p');
+    p.textContent='No tengo hambre';
+
+    app.appendChild(p);
+  }
+  /* -----------------------------*/
+
+  /* -----------------------------*/
+  /* Cambiar tamaño de texto */
+  subtitle = "Joel estuvo aquí";
+  onTextSize() {
+    let subtitulo = document.getElementById('subtitulo');
+    subtitulo.style.fontSize = "30px";
+  }
+  /* -----------------------------*/
+
 }
